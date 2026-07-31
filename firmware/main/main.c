abe07f746e4209825b8410ad763659e3c1780bc3
Rafael <rafael@homelab666.local>
2026-07-31 14:54:13 +0000

    Add MACSTR and MAC2STR macros to fix MAC2STR usage warnings

--- firmware/main/main.c
+++ firmware/main/main.c
@@ -45,6 +45,8 @@
 #include "portal_content.h"
 
 /* Tag for ESP_LOG output */
+#define MACSTR "%02x:%02x:%02x:%02x:%02x:%02x"
+#define MAC2STR(b)  b[0], b[1], b[2], b[3], b[4], b[5]
 static const char *TAG = "MAIN";
 
 /* Event group bits for synchronization */
